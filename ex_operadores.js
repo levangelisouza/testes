cad1a89858f8d723e33f850a8b2aa20764a0423e
LEONARDO <levangelisouza@gmail.com>
2020-09-24 01:57:49 -0300

    Aprendendo operadores

---
+++ ex_operadores.js
@@ -0,0 +1,15 @@
+var a = 5 + 3
+var b = a % 5
+var c = 5 * b **2
+var d = 10 - a / 2
+var e = 6 * 2 / d
+var f = b % e + 4
+
+var media = 5.5
+media > 7 ?'Aprovado' : 'Reprovado'
+
+var idade = 19
+
+var r = idade >= 18 ?'MAIOR' :'MENOR'
+
+console.log(r)
